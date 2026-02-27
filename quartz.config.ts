a3a84e2b499f00b596f9d615a5ecdb72da8955eb
Sasha (Win Git Bash) <flarexandra@gmail.com>
2026-02-27 01:15:48 +0100

    Quartz sync: Feb 27, 2026, 1:15 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -25,7 +25,7 @@ const config: QuartzConfig = {
       typography: {
         header: "Schibsted Grotesk",
         body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        code: "Victor Mono",
       },
       colors: {
         lightMode: {
